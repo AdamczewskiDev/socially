46e5336ef4316ac2787fe84b5bc78e81c65ca729
Damian <adamczewski.dev@gmail.com>
2025-10-28 13:40:48 +0100

    new user connected to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import { ModeToggle } from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1 className="text-3xl font-bold mb-4">Welcome to Socially!</h1>
